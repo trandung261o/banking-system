b0e1d18b92b76b6c83ce3fc024bbd58d8fbb6d1b
tranDung2610 <trandungtk321@gmail.com>
2023-06-14 03:42:47 +0700

    update

--- Banking System.cpp
+++ Banking System.cpp
@@ -4,6 +4,7 @@ int sum(int a, int b) {
 }
 int main() {
     int a, b;
+    //abc
     printf("input a: "); scanf("%d", &a);
     printf("input b: "); scanf("%d", &b);
     printf("sum of a and b is: %d", sum(a, b));
